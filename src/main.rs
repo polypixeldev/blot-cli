3b021c0df1c169641569c38e7400273ea795ac09
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-12-14 22:51:44 -0500

    Implement go command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+mod comms;
+use clap::{Parser, Subcommand};
+
+/// CLI for the Hack Club Blot
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+  #[command(subcommand)]
+  command: Option<Commands>,
+
+  #[arg(short, long)]
+  port: String,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Move the tool head to the specified coordinates
+    Go {
+        /// X coordinate
+        x: f32,
+        /// Y coordinate
+        y: f32
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+      Some(Commands::Go { x, y }) => {
+        println!("Going to: ({}, {})", x, y);
+        let mut payload: Vec<u8> = vec!();
+        payload.extend_from_slice(x.to_ne_bytes().as_slice());
+        payload.extend_from_slice(y.to_ne_bytes().as_slice());
+        let send_result = comms::send(cli.port, "go", payload, None);
+
+        if send_result.is_err() {
+          panic!("Failed to send message to Blot: {}", send_result.unwrap_err());
+        }
+
+        println!("Message sent to Blot");
+      }
+      None => {}
+  }
 }
